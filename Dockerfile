d5486d24b6f16fff701e011bb2405e26f05f9285
JM Acera <john.acera@sportality.se>
2024-04-02 14:21:30 +0800

    added try catch to functions, updated dockerfile and add npm run prod script

--- Dockerfile
+++ Dockerfile
@@ -2,18 +2,20 @@ FROM node:lts-alpine
 
 WORKDIR /usr/src/app
 
+COPY package.* .
+
+RUN npm install
+
 COPY . .
 
-# ARG MY_ENV_VARIABLE
-# ENV DOTA_SITE=$MY_ENV_VARIABLE
 ENV REDIS_STRING_URL=redis://redis:6379
 ENV OPEN_DOTA_API_URL=https://api.opendota.com/api
 ENV DOTA_SITE=http://localhost:8080
 ENV PORT=3000
 
-RUN npm install && npm run build
+RUN npm run build
 
 EXPOSE 3000
 
-CMD ["npm", "run", "start"]
+CMD ["npm", "run", "prod"]
 
--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -4,149 +4,193 @@ import { Hero } from '../dto/hero.dto';
 import { getRedis, setRedis } from "../cache/redis";
 import { Team } from "../dto/team.dto";
 
-
-export const getDotaHeroes = async (req: Request, res: Response, next: NextFunction) => {
-  const redisData = await getRedis('dotaHeroes');
-  if (redisData) {
-    return res.status(200).json(redisData);
+const logAndReturnError = (error: any, res: Response) => {
+  if (error.response) {
+    console.error('Error status:', error.response.status);
+    console.error('Error data:', error.response.data);
+    return res.status(500).json({ error: 'Error occurred' });
+  } else if (error.request) {
+    console.error('Request made but no response received:', error.request);
+    return res.status(500).json({ error: 'Error occurred' });
+  } else {
+    console.error('Error occurred:', error.message);
+    return res.status(500).json({ error: 'Error occurred' });
   }
-  const { query }: any = req;
-  const pageSize = query?.pageSize ?? 10;
-  const openDotaApiUrl: any = process.env.OPEN_DOTA_API_URL;
-  const { data } = await axios.get(`${openDotaApiUrl}/constants/heroes`);
-  const heroes = Object.values(data).map((hero: any) => {
-    return new Hero(hero);
-  });
-  await setRedis('dotaHeroes', heroes);
+}
+export const getDotaHeroes = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const redisData = await getRedis('dotaHeroes');
+    if (redisData) {
+      return res.status(200).json(redisData);
+    }
+    const { query }: any = req;
+    const pageSize = query?.pageSize ?? 10;
+    const openDotaApiUrl: any = process.env.OPEN_DOTA_API_URL;
+    const { data } = await axios.get(`${openDotaApiUrl}/constants/heroes`);
+    const heroes = Object.values(data).map((hero: any) => {
+      return new Hero(hero);
+    });
+    await setRedis('dotaHeroes', heroes);
 
-  return res.status(200).json(heroes);
+    return res.status(200).json(heroes);
+  } catch (error: any) {
+    logAndReturnError(error, res);
+  }
 }
 
 export const getHeroInfoById = async (req: Request, res: Response, next: NextFunction) => {
-  const { params }: any = req;
-  const id = parseInt(params?.id, 10);
-  const redisData = await getRedis(`dotaInfoHero-${id}`);
-  if (redisData) {
-    return res.status(200).json(redisData);
-  }
-  const openDotaApiUrl: any = process.env.OPEN_DOTA_API_URL;
-  const { data } = await axios.get(`${openDotaApiUrl}/constants/heroes`);
-  const hero: any = Object.values(data).find((hero: any) => {
-    return hero?.id === id;
-  });
-  await setRedis(`dotaInfoHero-${id}`, new Hero(hero));
+  try{
+    const { params }: any = req;
+    const id = parseInt(params?.id, 10);
+    const redisData = await getRedis(`dotaInfoHero-${id}`);
+    if (redisData) {
+      return res.status(200).json(redisData);
+    }
+    const openDotaApiUrl: any = process.env.OPEN_DOTA_API_URL;
+    const { data } = await axios.get(`${openDotaApiUrl}/constants/heroes`);
+    const hero: any = Object.values(data).find((hero: any) => {
+      return hero?.id === id;
+    });
+    await setRedis(`dotaInfoHero-${id}`, new Hero(hero));
 
-  return res.status(200).json(new Hero(hero));
+    return res.status(200).json(new Hero(hero));
+  } catch (error: any) {
+    logAndReturnError(error, res);
+  }
 }
 
 export const getHeroMatchupById = async (req: Request, res: Response, next: NextFunction) => {
-  const { params }: any = req;
-  const id = parseInt(params?.id, 10);
-  const redisData = await getRedis(`dotaHeroMatchup-${id}`);
-  if (redisData) {
-    return res.status(200).json(redisData);
-  }
-  const openDotaApiUrl: any = process.env.OPEN_DOTA_API_URL;
-  const dotaHeroMatchupsRaw = await axios.get(`${openDotaApiUrl}/heroes/${id}/matchups`);
-  const { data } = await axios.get(`${openDotaApiUrl}/constants/heroes`);
-  const heroes = Object.values(data).map((hero: any) => {
-    return new Hero(hero);
-  });
-  const dotaHeroMatchups = dotaHeroMatchupsRaw?.data.map((dotaHeroMatchupRaw: any) => (
-    {
-      id: dotaHeroMatchupRaw?.hero_id,
-      name: heroes?.find((hero) => (hero.id === dotaHeroMatchupRaw?.hero_id))?.name,
-      img: heroes?.find((hero) => (hero.id === dotaHeroMatchupRaw?.hero_id))?.img,
-      wins: dotaHeroMatchupRaw?.wins,
-      gamesPlayed: dotaHeroMatchupRaw?.games_played,
-      winRate: dotaHeroMatchupRaw?.wins/dotaHeroMatchupRaw?.games_played * 100,
+  try {
+    const { params }: any = req;
+    const id = parseInt(params?.id, 10);
+    const redisData = await getRedis(`dotaHeroMatchup-${id}`);
+    if (redisData) {
+      return res.status(200).json(redisData);
     }
-  )).sort((a: any, b: any) => b.winRate - a.winRate);
-  await setRedis(`dotaHeroMatchup-${id}`, dotaHeroMatchups)
+    const openDotaApiUrl: any = process.env.OPEN_DOTA_API_URL;
+    const dotaHeroMatchupsRaw = await axios.get(`${openDotaApiUrl}/heroes/${id}/matchups`);
+    const { data } = await axios.get(`${openDotaApiUrl}/constants/heroes`);
+    const heroes = Object.values(data).map((hero: any) => {
+      return new Hero(hero);
+    });
+    const dotaHeroMatchups = dotaHeroMatchupsRaw?.data.map((dotaHeroMatchupRaw: any) => (
+      {
+        id: dotaHeroMatchupRaw?.hero_id,
+        name: heroes?.find((hero) => (hero.id === dotaHeroMatchupRaw?.hero_id))?.name,
+        img: heroes?.find((hero) => (hero.id === dotaHeroMatchupRaw?.hero_id))?.img,
+        wins: dotaHeroMatchupRaw?.wins,
+        gamesPlayed: dotaHeroMatchupRaw?.games_played,
+        winRate: dotaHeroMatchupRaw?.wins/dotaHeroMatchupRaw?.games_played * 100,
+      }
+    )).sort((a: any, b: any) => b.winRate - a.winRate);
+    await setRedis(`dotaHeroMatchup-${id}`, dotaHeroMatchups)
 
-  return res.status(200).json(dotaHeroMatchups);
+    return res.status(200).json(dotaHeroMatchups);
+  } catch (error: any) {
+    logAndReturnError(error, res);
+  }
 }
 
 export const getProplayers = async (req: Request, res: Response, next: NextFunction) => {
-  const redisData = await getRedis('proPlayers');
-  if (redisData) {
-    return res.status(200).json(redisData);
-  }
-  
-  const openDotaApiUrl: any = process.env.OPEN_DOTA_API_URL;
-  const { data } = await axios.get(`${openDotaApiUrl}/proPlayers`);
-  await setRedis('proPlayers', data)
+  try {
+    const redisData = await getRedis('proPlayers');
+    if (redisData) {
+      return res.status(200).json(redisData);
+    }
+    
+    const openDotaApiUrl: any = process.env.OPEN_DOTA_API_URL;
+    const { data } = await axios.get(`${openDotaApiUrl}/proPlayers`);
+    await setRedis('proPlayers', data)
 
-  return res.status(200).json(data);
+    return res.status(200).json(data);
+  } catch (error: any) {
+    logAndReturnError(error, res);
+  }
 }
 
 export const getProTeams = async (req: Request, res: Response, next: NextFunction) => {
-  const redisData = await getRedis('proTeams');
-  if (redisData) {
-    return res.status(200).json(redisData);
+  try {
+    const redisData = await getRedis('proTeams');
+    if (redisData) {
+      return res.status(200).json(redisData);
+    }
+    const openDotaApiUrl: any = process.env.OPEN_DOTA_API_URL;
+    const { data } = await axios.get(`${openDotaApiUrl}/teams`);
+    const teamData = data?.map((item: any) => ({
+      ...item,
+      hoverFirst: item?.rating,
+      hoverSecond: item?.wins,
+      hoverThird: item?.losses,
+      id: item?.team_id,
+      img: item?.logo_url,
+    }))
+    await setRedis('proTeams', teamData)
+    return res.status(200).json(teamData);
+  } catch (error: any) {
+    logAndReturnError(error, res);
   }
-  const openDotaApiUrl: any = process.env.OPEN_DOTA_API_URL;
-  const { data } = await axios.get(`${openDotaApiUrl}/teams`);
-  const teamData = data?.map((item: any) => ({
-    ...item,
-    hoverFirst: item?.rating,
-    hoverSecond: item?.wins,
-    hoverThird: item?.losses,
-    id: item?.team_id,
-    img: item?.logo_url,
-  }))
-  await setRedis('proTeams', teamData)
-  return res.status(200).json(teamData);
 }
 
 const groupBy = (objectArray: any, property: any) => {
-  return objectArray.reduce((accumulator: any, currentObject: any) => {
-    let key = currentObject[property];
-    if (!accumulator[key]) {
-      accumulator[key] = {
-        id: 0,
-        wins: 0,
-        gamesPlayed: 0,
-        winRate: 0,
-        name: '',
-        img: '',
-        leagueName: '',
-      };
-    }
-    accumulator[key].id = currentObject.opposing_team_id;
-    accumulator[key].wins = currentObject.radiant === currentObject.radiant_win ? accumulator[key].wins + 1 : accumulator[key].wins;
-    accumulator[key].gamesPlayed += 1;
-    accumulator[key].winRate = accumulator[key].wins/accumulator[key].gamesPlayed * 100;
-    accumulator[key].name = currentObject.opposing_team_name;
-    accumulator[key].img = currentObject.opposing_team_logo;
-    accumulator[key].leagueName = currentObject.league_name;
-    return accumulator;
-  }, {});
+  try {
+    return objectArray.reduce((accumulator: any, currentObject: any) => {
+      let key = currentObject[property];
+      if (!accumulator[key]) {
+        accumulator[key] = {
+          id: 0,
+          wins: 0,
+          gamesPlayed: 0,
+          winRate: 0,
+          name: '',
+          img: '',
+          leagueName: '',
+        };
+      }
+      accumulator[key].id = currentObject.opposing_team_id;
+      accumulator[key].wins = currentObject.radiant === currentObject.radiant_win ? accumulator[key].wins + 1 : accumulator[key].wins;
+      accumulator[key].gamesPlayed += 1;
+      accumulator[key].winRate = accumulator[key].wins/accumulator[key].gamesPlayed * 100;
+      accumulator[key].name = currentObject.opposing_team_name;
+      accumulator[key].img = currentObject.opposing_team_logo;
+      accumulator[key].leagueName = currentObject.league_name;
+      return accumulator;
+    }, {});
+  } catch (error) {
+    console.error('Error status:', JSON.stringify(error));
+  }
 }
 
 export const getTeamMatchupByTeamId = async (req: Request, res: Response, next: NextFunction) => {
-  const { params }: any = req;
-  const id = parseInt(params?.id, 10);
-  const redisData = await getRedis(`dotaTeamMatchup-${id}`);
-  if (redisData) {
-    return res.status(200).json(redisData);
+  try {
+    const { params }: any = req;
+    const id = parseInt(params?.id, 10);
+    const redisData = await getRedis(`dotaTeamMatchup-${id}`);
+    if (redisData) {
+      return res.status(200).json(redisData);
+    }
+    const openDotaApiUrl: any = process.env.OPEN_DOTA_API_URL;
+    const { data } = await axios.get(`${openDotaApiUrl}/teams/${id}/matches`);
+    const teamMatchup = Object.values(groupBy(data, 'opposing_team_id'));
+    await setRedis(`dotaTeamMatchup-${id}`, teamMatchup);
+    return res.status(200).json(teamMatchup);
+  } catch (error: any) {
+    logAndReturnError(error, res);
   }
-  const openDotaApiUrl: any = process.env.OPEN_DOTA_API_URL;
-  const { data } = await axios.get(`${openDotaApiUrl}/teams/${id}/matches`);
-  const teamMatchup = Object.values(groupBy(data, 'opposing_team_id'));
-  await setRedis(`dotaTeamMatchup-${id}`, teamMatchup);
-  return res.status(200).json(teamMatchup);
 }
 
 export const getTeamInfoByTeamId = async (req: Request, res: Response, next: NextFunction) => {
-  const { params }: any = req;
-  const id = parseInt(params?.id, 10);
-  const redisData = await getRedis(`dotaInfoTeam-${id}`);
-  if (redisData) {
-    return res.status(200).json(redisData);
+  try {
+    const { params }: any = req;
+    const id = parseInt(params?.id, 10);
+    const redisData = await getRedis(`dotaInfoTeam-${id}`);
+    if (redisData) {
+      return res.status(200).json(redisData);
+    }
+    const openDotaApiUrl: any = process.env.OPEN_DOTA_API_URL;
+    const { data } = await axios.get(`${openDotaApiUrl}/teams/${id}`);
+    await setRedis(`dotaInfoTeam-${id}`, new Team(data));
+    return res.status(200).json(new Team(data));
+  } catch (error: any) {
+    logAndReturnError(error, res);
   }
-  const openDotaApiUrl: any = process.env.OPEN_DOTA_API_URL;
-  const { data } = await axios.get(`${openDotaApiUrl}/teams/${id}`);
-  await setRedis(`dotaInfoTeam-${id}`, new Team(data));
-  return res.status(200).json(new Team(data));
 }
